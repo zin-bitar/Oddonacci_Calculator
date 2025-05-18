954391c91fab3638b4ef181aed227255a53731fd
zin-bitar <zinbitar1469@gmail.com>
2025-05-18 03:26:32 -0400

    timing2

--- Driver.java
+++ Driver.java
@@ -5,25 +5,34 @@ public class Driver {
 
         File oddText = new File("OddoOut.txt");
         PrintWriter oddWriter;
-        File timing = new File("Timing.txt");
+        File timingText = new File("Timing.txt");
+        PrintWriter timingWriter;
         try {
             oddWriter = new PrintWriter(new FileOutputStream(oddText));
+            timingWriter = new PrintWriter(new FileOutputStream(timingText));
 
             int k = 0;
             for (int i = 0; i < 5; i++) {
                 k +=5;
+                long time = System.nanoTime();
                 int[] linear = GoodOdd(k);
+                long timeDifference = System.nanoTime()-time;
                 oddWriter.print("Linear for " + k + ": " + linear[0] + "\n");
+                timingWriter.print("linear " + k+" took: " + timeDifference + " nanoseconds\n" );
             }
 
             oddWriter.print("\n");
+            timingWriter.print("\n");
 
             k = 0;
             for (int i = 0; i < 5; i++) {
                 k +=5;
+                long time2 = System.nanoTime();
                 oddWriter.print("Ternary for " + k + ": " + BadOdd(k) + "\n");
+                timingWriter.print("ternary " + k+" took: " + (System.nanoTime()-time2) + " nanoseconds\n");
             }
 
+            timingWriter.close();
             oddWriter.close();
 
         } catch (FileNotFoundException e) {
